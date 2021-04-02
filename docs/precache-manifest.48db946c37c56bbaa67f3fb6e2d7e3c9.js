3867befe7df6c0e57c35c0229413506bab79ebb9
lindelof <phragmen@protonmail.com>
2021-04-02 17:16:49 +0800

    Add doc

---
+++ docs/precache-manifest.48db946c37c56bbaa67f3fb6e2d7e3c9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c98546d20295930c7c07165792a612e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7238deded88ec4266c3",
+    "url": "./static/css/main.832842c6.chunk.css"
+  },
+  {
+    "revision": "6da0c9e9e26b06eaf33e",
+    "url": "./static/js/2.7ef07b5a.chunk.js"
+  },
+  {
+    "revision": "f777c2cd8d43a44e423cc24a2b4ae71a",
+    "url": "./static/js/2.7ef07b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7238deded88ec4266c3",
+    "url": "./static/js/main.9873de18.chunk.js"
+  },
+  {
+    "revision": "53c8213d7beab5c9be14",
+    "url": "./static/js/runtime-main.edec85e9.js"
+  },
+  {
+    "revision": "b64a560c149246ae0499d5c8b69ffff6",
+    "url": "./static/media/1.b64a560c.jpg"
+  },
+  {
+    "revision": "ff53d14652d9154a4abbca88aaca338c",
+    "url": "./static/media/2.ff53d146.jpg"
+  },
+  {
+    "revision": "1e09e2a9174793bcf5a829caaa701f57",
+    "url": "./static/media/3.1e09e2a9.jpg"
+  },
+  {
+    "revision": "98b8e8b269d078d764c73e81de4f2e00",
+    "url": "./static/media/4.98b8e8b2.jpg"
+  },
+  {
+    "revision": "e1e3e877a81f50a4832734ddc2a0d435",
+    "url": "./static/media/5.e1e3e877.jpg"
+  },
+  {
+    "revision": "501473f45f90d943cd3aeb5d553ce56c",
+    "url": "./static/media/6.501473f4.jpg"
+  },
+  {
+    "revision": "45bcb7d775d1dce4c70acb12ce907558",
+    "url": "./static/media/7.45bcb7d7.jpg"
+  },
+  {
+    "revision": "1e09e2a9174793bcf5a829caaa701f57",
+    "url": "./static/media/8.1e09e2a9.jpg"
+  },
+  {
+    "revision": "00aef28ea1c7d18022f14abe3f55167e",
+    "url": "./static/media/9.00aef28e.jpg"
+  },
+  {
+    "revision": "36dc7128285ef7e2025edbf4f650ebaa",
+    "url": "./static/media/head.36dc7128.jpg"
+  }
+]);
